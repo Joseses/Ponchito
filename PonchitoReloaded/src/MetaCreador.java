f09c8ef86df504abf7b746c40b04c734e7934347
Josema0 <cosemine.josema@gmail.com>
2015-12-03 15:03:13 -0600

    MetaCreador

---
+++ PonchitoReloaded/src/MetaCreador.java
@@ -0,0 +1,12 @@
+public class MetaCreador {
+    Puente puente;
+
+    public MetaCreador(Puente puente) {
+        this.puente = puente;
+    }
+
+    public LugarVisitar crearLugar() {
+
+        return null;
+    }
+}
