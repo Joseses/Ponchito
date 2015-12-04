2b073fde7639d9267d3e78358ab033656646bf0a
JoseOm07 <beto_bcw_09@hotmail.com>
2015-12-04 01:22:12 -0600

    meta circuito

--- PonchitoReloaded/src/MetaCreador.java
+++ PonchitoReloaded/src/MetaCreador.java
@@ -18,15 +18,14 @@ public class MetaCreador {
                     rs.getString("pais"), rs.getString("direccion"), rs.getString("descripcion"),
                     rs.getDouble("precio"));
             lugares.add(lugar);
-            System.out.println("Hola");
         }
         return lugares;
     }
-    
+
     public ArrayList<Hotel> crearHoteles() throws SQLException {
         ArrayList<Hotel> hoteles = new ArrayList<Hotel>();
-        ResultSet rs = puente.getQueryResults("Select * From hotel left join localizacion\n" +
-                "on localizacion.idloc = hotel.localizacion;");
+        ResultSet rs = puente.getQueryResults("Select  *\n" +
+                "from ciudad natural join hotel;");
         while(rs.next()) {
             Hotel hotel = new Hotel(rs.getString("nombreHotel"), rs.getString("ciudad"), rs.getString("pais"),
                     rs.getString("direccion"), rs.getInt("numCuartos"), rs.getDouble("precioCuarto"),
@@ -38,20 +37,12 @@ public class MetaCreador {
 
     public ArrayList<Circuito> crearCircuitos() throws SQLException {
         ArrayList<Circuito> circuitos = new ArrayList<Circuito>();
-        ResultSet rs = puente.getQueryResults("Select idcircuito,descripcion,lugarSalida,lugarLlegada,duracion,precio,\n" +
-                "l1.ciudad as ciudad1,\n" +
-                "l1.pais as pais1, l2.ciudad as ciudad2,l2.pais as pais2 \n" +
-                "From circuito natural join (localizacion l1, localizacion l2) \n" +
-                "where l1.idloc = circuito.lugarSalida and l2.idloc = circuito.lugarLlegada;");
+        ResultSet rs = puente.getQueryResults("Select  *\n" +
+                "from circuito;");
         while(rs.next()) {
-            String[] loc1 = new String[2];
-            String[] loc2 = new String[2];
-            loc1[0] = rs.getString("ciudad1");
-            loc1[1] = rs.getString("pais1");
-            loc2[0] = rs.getString("ciudad2");
-            loc2[1] = rs.getString("pais2");
-            Circuito circuito = new Circuito(rs.getInt("idcircuito"), rs.getString("descripcion"),loc1, loc2,
-                    rs.getString("duracion"), rs.getInt("precio"));
+            Circuito circuito = new Circuito(rs.getInt("idcircuito"), rs.getString("descripcion"),rs.getString("ciudadSalida"),
+                    rs.getString("paisSalidad"),rs.getString("ciudadLlegada"),rs.getString("paisLlegada"), rs.getString("duracion"),
+                    rs.getInt("precio"));
             circuitos.add(circuito);
         }
         return circuitos;
